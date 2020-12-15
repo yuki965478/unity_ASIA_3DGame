36ce7ef5681194b4705a33e6dd7db4a101c9fdb6
yuki965478 <71679934+yuki965478@users.noreply.github.com>
2020-12-15 16:00:05 +0800

    對話程式

---
+++ 3DGame/Assets/Script/NPC.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCDdata data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+
+    //<summary>
+    //玩家是否進入感應區
+    //</summary>
+
+    public bool playerInArea;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name=="小名")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name=="小名")
+        {
+            playerInArea = false;
+        }
+    }
+    private void Dialog()
+    {
+        //print(data.dialougA);//取得字串全部資料
+
+
+        ////for 迴圈：重複處理相同程式
+        //for(int i = 0; i < 10; i++) 
+        //{
+        //    print("我是迴圈" + i);
+        //}
+
+        //字串的長度：dialogA.Length
+        for (int i = 0; i < data.dialougA.Length; i++)
+        {
+            print(data.dialougA[i]);
+        }
+    }
+}
---
+++ 3DGame/Assets/Script/NPCDdata.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+//ScriptableObject 腳本化物件
+//將腳本資料變成物件並保存在專案內
+
+    //建立資源選單("檔案名稱","選單名稱")
+    [CreateAssetMenu(fileName ="NPC資料",menuName ="Avocado/NPC")]
+public class NPCDdata : ScriptableObject
+{
+    [Header("第一段對話"),TextArea(1,5)]
+    public string dialougA;
+    [Header("第二段對話"),TextArea(1,5)]
+    public string dialougB;
+    [Header("第三段對話"),TextArea(1,5)]
+    public string dialougC;
+    [Header("任務項目需求")]
+    public int count;
+    [Header("已經取得項目數量")]
+    public int countCurrent;
+}
